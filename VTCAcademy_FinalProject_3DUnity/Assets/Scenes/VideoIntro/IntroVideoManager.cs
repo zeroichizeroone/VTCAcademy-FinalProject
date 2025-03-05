de7c731c15d4a6857726953a22a827198c04653e
Thanh Kiet <115813076+Vikendi2249@users.noreply.github.com>
2025-03-05 15:08:11 +0700

    update
    introgame

---
+++ VTCAcademy_FinalProject_3DUnity/Assets/Scenes/VideoIntro/IntroVideoManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class IntroVideoManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;  // Kéo VideoPlayer vào Inspector
+    public string nextSceneName = "MainMenu";  // Tên scene tiếp theo
+
+    private void Start()
+    {
+        // Đăng ký sự kiện khi video kết thúc
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        // Chuyển scene sau khi video kết thúc
+        SceneManager.LoadScene("MainMenu");
+    }
+}
